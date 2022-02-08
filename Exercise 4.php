7f1a08b32f31bc123a5840540b07bd75f410edcf
Francis Boafo <97594225+Franxbj@users.noreply.github.com>
2022-02-08 14:58:19 +0200

    Exercise 1, 2

---
+++ Exercise 4.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 4 - Francis Boafo</title>
+</head>
+<body>
+<style>
+    h1 {
+        background-color: #D4D4D4; 
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
+<?php
+    echo "<h1>QUESTION 1</h1>";
+?>
+
+<?php
+$courses = array("PHP", "HTML", "Javascript", "CMS", "project");
+
+echo "
+
+    <li>$courses[0]</li>
+    <li>$courses[1]</li>
+    <li>$courses[2]</li>
+    <li>$courses[3]</li>
+    <li>$courses[4]</li>
+   
+ ";
+?>
+
+<?php
+    echo "<h1>QUESTION 2</h1>";
+?>
+
+<?php
+$courses1 = array("PHP", "HTML", "Javascript", "CMS", "project");
+echo "Elements in the Array - Courses1 <br>";
+echo $courses1[0] .", ".  $courses1[1] . ", ". $courses1[2] .", ".  $courses1[3] . ", ". $courses1[4];
+
+// lets remove HTML
+echo "<br>";
+echo "<br>";
+print_r($courses1);
+echo "<br>";
+echo "<br>";
+echo "Using unset() to remove HTML, the final result is <br>";
+
+unset($courses1[1]);
+
+print_r($courses1);
+
+?>
+
+<?php
+    echo "<h1>QUESTION 3</h1>";
+?>
+
+</body>
+</html>
